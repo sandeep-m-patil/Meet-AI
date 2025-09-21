c815a4d7c878c2ac13f51c44048f5ab424f8e933
sandeep <sandeeppatil10705@gmail.com>
2025-09-21 14:33:10 +0530

    Add social authentication options for Google and GitHub in SignIn and SignUp views

--- modules/auth/ui/views/signup-view.tsx
+++ modules/auth/ui/views/signup-view.tsx
@@ -148,6 +148,50 @@ export default function SignUpView() {
               </form>
             </Form>
 
+  <div className="flex items-center gap-3 my-3">
+              <div className="flex-1 h-px bg-gray-200" />
+              <div className="text-sm text-gray-500">or</div>
+              <div className="flex-1 h-px bg-gray-200" />
+            </div>
+            <div className="space-y-5">
+              <div className="flex flex-col sm:flex-row gap-3">
+                <Button
+                  variant="outline"
+                  className="flex-1 flex items-center justify-center gap-2"
+                  onClick={() => {
+                    authClient.signIn.social({ provider: 'google' });
+                  }}
+                >
+                  <span className="inline-flex items-center justify-center w-5 h-5">
+                    <svg viewBox="0 0 48 48" className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                      <path fill="#4285F4" d="M24 9.5c3.9 0 7 1.5 9.4 3.5l7-7C35 2.1 29.9 0 24 0 14.9 0 6.9 4.6 2.6 11.4l8.1 6.3C12.9 13 18 9.5 24 9.5z" />
+                      <path fill="#34A853" d="M46.5 24.5c0-1.6-.1-3.1-.4-4.6H24v9.1h12.7c-.5 2.7-2 5-4.3 6.6l6.7 5.2C43.8 38 46.5 31.8 46.5 24.5z" />
+                      <path fill="#FBBC05" d="M10.7 30.4A14.8 14.8 0 0 1 9.5 24c0-1.2.2-2.5.5-3.6L2 13.9C.7 16.5 0 20.1 0 24c0 3.9.7 7.5 2 10.9l8.7-4.5z" />
+                      <path fill="#EA4335" d="M24 48c6.5 0 12-2.1 16-5.7l-7.7-6.1C29.9 36.4 27 37.5 24 37.5 18 37.5 12.9 34 10.7 30.4l-8.7 4.5C6.9 43.4 14.9 48 24 48z" />
+                    </svg>
+                  </span>
+                  <span>Continue with Google</span>
+                </Button>
+
+                <Button
+                  variant="outline"
+                  className="flex-1 flex items-center justify-center gap-2"
+                  onClick={() => {
+                    authClient.signIn.social({ provider: 'github' });
+                  }}
+                >
+                  <span className="inline-flex items-center justify-center w-5 h-5">
+                    <svg viewBox="0 0 16 16" className="w-4 h-4" xmlns="http://www.w3.org/2000/svg">
+                      <path fillRule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.01.08-2.1 0 0 .67-.22 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.09.16 1.9.08 2.1.51.56.82 1.28.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38C13.71 14.53 16 11.54 16 8c0-4.42-3.58-8-8-8z" />
+                    </svg>
+                  </span>
+                  <span>Continue with GitHub</span>
+                </Button>
+              </div>
+
+
+            </div>
+            
             <div className="mt-6 text-center">
               <p className="text-sm text-gray-600">
                 Already have an account?{' '}
